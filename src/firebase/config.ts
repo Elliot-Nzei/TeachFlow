b1cb6d5e02f3c75868c8fc25e4ef9d1f44fbe82f
Elliot Nzei <elliotnzei41@gmail.com>
2025-10-10 17:41:30 +0000

    1/1
    Next.js 15.3.3 (stale)
    Turbopack
    Console Error
    
    FirebaseError: Fir

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:244101903357:web:00d1dc8e47d3cc771e9f94",
   "apiKey": "AIzaSyCPddm-nIjQRvZBzYwCHgRFcrZkgIGqnBo",
   "authDomain": "studio-985481692-56aea.firebaseapp.com",
+  "storageBucket": "studio-985481692-56aea.appspot.com",
   "measurementId": "",
   "messagingSenderId": "244101903357"
 };
