e016fe18f0f41b05b3713c01cf804fb0e65bc385
Elliot Nzei <elliotnzei41@gmail.com>
2025-10-18 17:46:00 +0000

    check for errors

--- src/next.config.ts
+++ src/next.config.ts
@@ -37,8 +37,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-        ,
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
