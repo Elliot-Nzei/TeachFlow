a828f674df13045579d5330915d169d6dbad879b
Elliot Nzei <elliotnzei41@gmail.com>
2025-10-23 07:22:55 +0000

    is everything ready now
    studio-985481692:~/studio{main}$ npm run dev

--- src/next.config.ts
+++ src/next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: [
+        "https://3000-firebase-studio-1759929584345.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev",
+        "https://6000-firebase-studio-1759929584345.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev",
+    ]
+  },
   async headers() {
     return [
       {
