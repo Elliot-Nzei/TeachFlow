54f2d92f78c6ed5a3bb341e65d42f012450769cb
Elliot Nzei <elliotnzei41@gmail.com>
2025-10-24 10:53:24 +0000

    then delete this /home/user/studio/src/app/(admin)
    lets use this from n

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,5 +1,5 @@
 // This file is intentionally left blank.
-// The admin section has been moved to /src/app/admin/ to fix routing issues.
+// The (admin) route group has been deprecated and moved to /src/app/admin/ to fix routing issues.
 export default function RemovedAdminLayout() {
   return null;
 }
