151f7d92023c91e101c3faa8b34ed58397e1450a
Elliot Nzei <elliotnzei41@gmail.com>
2025-10-24 09:22:06 +0000

    1/1
    Next.js 0.0.0 (unknown)
    Webpack
    Runtime Error
    
    Error: You cannot ha

--- src/app/(admin)/settings/page.tsx
+++ src/app/(admin)/settings/page.tsx
@@ -1,3 +1,4 @@
+
 // This file has been renamed to page.tsx.bak to resolve a routing conflict.
 // The correct user settings page is at /src/app/(app)/settings/page.tsx
 // The correct admin settings page is at /src/app/(admin)/admin/settings/page.tsx
