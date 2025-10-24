65314b751c189def9bad305cfde4c10b2a1b278d
Elliot Nzei <elliotnzei41@gmail.com>
2025-10-24 10:51:05 +0000

    question? so we are using this
    /home/user/studio/src/app/admin
    
    instea

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,5 @@
+// This file is intentionally left blank.
+// The admin section has been moved to /src/app/admin/ to fix routing issues.
+export default function RemovedAdminLayout() {
+  return null;
+}
